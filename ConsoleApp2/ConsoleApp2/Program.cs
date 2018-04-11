87f042dac81caa8d21b6de972cbeac706c54109d
Shawheen93 <35010811+Shawheen93@users.noreply.github.com>
2018-04-10 19:32:34 -0700

    Initial Commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Year");
+            int Year = 2018;
+            Console.WriteLine(Year);
+            Console.WriteLine("Which student?");
+            string Name = "Student 1";
+            Console.WriteLine(Name);
+            Console.WriteLine("Is Student 1 Enrolled?");
+            bool Enrolled = true; Console.WriteLine("Student 1 is enrolled"); 
+            Console.WriteLine("What is age of Student 1?");
+            int Age = 28;
+            Console.WriteLine(Age);
+            
+            
+            
+
+
+
+        }
+       
+    }
+}
